4e17fdcd4e796fbd0793126640dd2ebcfd503864
Corey Williams <cmw6pa@virginia.edu>
2019-07-03 17:08:33 -0400

    Add function to read Concat_Transformed

--- R/FileInput.R
+++ R/FileInput.R
@@ -1,4 +1,4 @@
-#' Read metadata
+#' Metadata file input
 #'
 #' Reads metadata file and produces format friendly to scripts in Zunder lab pipeline. Metadata
 #' strategy inspired by Nowicka et al., 2017, F1000 Research
@@ -12,16 +12,27 @@ read.metadata <- function(input.folder,md.filename = "metadata.csv"){
   return(md)
 }
 
-#' Read panel
+#' Panel file input
 #'
 #' Reads panel file and produces format friendly to scripts in Zunder lab pipeline
 #' @param input.folder directory containing metadata file
-#' @param panel.filename panel filename, defaults to metadata.csv as outputted by generation script
+#' @param panel.filename panel filename, defaults to panel.csv as outputted by generation script
 #' @export
 read.panel <- function(input.folder,panel.filename = "panel.csv"){
   panel <- read.csv(paste0(input.folder,panel.filename))
   panel$Antigen <- gsub("-", "_", panel$Antigen)
   return(panel)
+}
+
+#' Concat_Transformed file input
+#'
+#' Reads Concat_Transformed file for use in Zunder lab pipeline
+#' @param input.folder directory containing metadata file
+#' @param concat.transformed.filename concat_transformed filename, defaults to
+#' Concat_Transformed.csv as outputted by generation script
+#' @export
+read.concat.transformed <- function(input.folder,concat.transformed.filename = "Concat_Tranformed.csv"){
+  concat.transformed <- read.csv(paste0(input.folder,concat.transformed.filename))
 }
 
 #' Pull out markers for transformation
