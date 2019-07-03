6088f10695f3d35daeaaec191fb3cd43a6d955a1
Corey Williams <cmw6pa@virginia.edu>
2019-07-03 11:34:17 -0400

    Add functions to pull markers for asinh transformation

--- R/FileInput.R
+++ R/FileInput.R
@@ -21,3 +21,21 @@ read.panel <- function(input.folder,panel.filename = "panel.csv"){
   panel$Antigen <- gsub("-", "_", panel$Antigen)
   return(panel)
 }
+
+#' Pull out markers for transformation
+#'
+#' Finds markers in panel file marked for either clustering or plotting
+#' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' function
+#' @export
+get.transform.markers <- function(panel){
+  transform.markers <- panel$Metal[panel$Clustering == 1 || panel$Plotting == 1]
+}
+
+#' Get marker names in more legibile format for Concat_Transformed file
+#' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' function
+#' @export
+get.transform.annotate <- function(panel){
+  transform.markers.annotate <- paste0(panel$Antigen[panel$Clustering == 1 || panel$Plotting == 1],"_",panel$Metal[panel$Clustering == 1 || panel$Plotting == 1])
+}
