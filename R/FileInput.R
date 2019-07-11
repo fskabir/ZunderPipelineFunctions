cc617d8fb292d1140be037853d92e4971a1f7875
Corey Williams <cmw6pa@virginia.edu>
2019-07-11 15:14:22 -0400

    Add function to pull clustering variables out of panel

--- R/FileInput.R
+++ R/FileInput.R
@@ -54,3 +54,12 @@ get.transform.annotate <- function(panel){
   transform.markers.annotate <- paste0(panel$Antigen[panel$Clustering == 1 | panel$Plotting == 1],"_",panel$Metal[panel$Clustering == 1 | panel$Plotting == 1])
   return(transform.markers.annotate)
 }
+
+#' Get clustering variables in legible format
+#' #' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' function
+#' @export
+get.clustering.annotate <- function(panel){
+  clustering.markers.annotate <- paste0(panel$Antigen[panel$Clustering == 1],"_",panel$Metal[panel$Clustering == 1])
+  return(clustering.markers.annotate)
+}
