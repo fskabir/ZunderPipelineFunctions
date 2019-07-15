7985ff4f81ba4eacfb153b2f6b14ee194931b68c
Corey Williams <cmw6pa@virginia.edu>
2019-07-15 12:41:46 -0400

    Add function to access plotting markers

--- R/AccessPanel.R
+++ R/AccessPanel.R
@@ -14,15 +14,28 @@ get.transform.markers <- function(panel){
 #' function
 #' @export
 get.transform.annotate <- function(panel){
-  transform.markers.annotate <- paste0(panel$Antigen[panel$Clustering == 1 | panel$Plotting == 1],"_",panel$Metal[panel$Clustering == 1 | panel$Plotting == 1])
+  transform.markers.annotate <- paste0(panel$Antigen[panel$Clustering == 1 | panel$Plotting == 1],
+                                       "_",panel$Metal[panel$Clustering == 1 |
+                                                         panel$Plotting == 1])
   return(transform.markers.annotate)
 }
 
 #' Get clustering variables in legible format
-#' #' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' @param panel panel formatted following generation by getMetadata.R and input by read.panel
 #' function
 #' @export
 get.clustering.annotate <- function(panel){
-  clustering.markers.annotate <- paste0(panel$Antigen[panel$Clustering == 1],"_",panel$Metal[panel$Clustering == 1])
+  clustering.markers.annotate <- paste0(panel$Antigen[panel$Clustering == 1],"_",
+                                        panel$Metal[panel$Clustering == 1])
   return(clustering.markers.annotate)
+}
+
+#' Get panel variables in legible format
+#' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' function
+#' @export
+get.plotting.annotate <- function(panel){
+  plotting.markers.annotate <- paste0(panel$Antigen[panel$Plotting == 1],"_",
+                                      panel$Metal[panel$Plotting == 1])
+  return(plotting.markers.annotate)
 }
